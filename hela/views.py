970e9e4c24a167a204cf91054071891009ebe3ce
jorjo19fe <mrgiurgiusjorjo@gmail.com>
2023-02-13 00:47:09 +0400

    update1

--- hela/views.py
+++ hela/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
-def index(request):
-    return HttpResponse("hello man")
+
+
+def index(request, name, mama):
+    mama=0
+    return render(request, "hela/index.html", {
+        "name": name,
+        "mama": mama
+    })
